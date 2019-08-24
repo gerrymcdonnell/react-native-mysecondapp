e81765c8528f2dda99c2f8bbb84185d09e4d42b9
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-08-24 21:02:53 +0100

    added simple text inputbox

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View,TextInput } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -7,12 +7,29 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+
+  state={
+    placeName:''
+  }
+
+  
+  placeBameChangedHandler=(event)=>{
+    alert(event);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Hello GED Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+      
+        <TextInput 
+          style={{width:300,borderColor:"black",borderWidth:1}}
+          value={this.state.container}
+          onChangeText={this.placeBameChangedHandler}          
+        />
+
       </View>
     );
   }
